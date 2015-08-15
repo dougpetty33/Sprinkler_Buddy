146743ea9d1d005a3c3028d4745b827cde05efd5
Doug Petty <dougpetty33@gmail.com>
2015-08-15 14:48:59 -0600

    Service discovery implemented

--- sprinkler_controller/sprinkler_controller.ino
+++ sprinkler_controller/sprinkler_controller.ino
@@ -5,6 +5,7 @@
 #include <avr/pgmspace.h>
 #include <avr/wdt.h>
 #include <Ethernet.h>
+#include <EthernetUdp.h>
 #include <SPI.h>
 #include <TextFinder.h>
 #include <EEPROM.h>
@@ -15,68 +16,28 @@
 //need to find a way to be able to modify values inline
 //possibly a function that serves up html appropriately
 
-const char string_0[] PROGMEM = "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js\">";
-const char string_1[] PROGMEM = "</script><script>var url = \"192.168.0.5\"; $(document).ready(function(){";
-const char string_2[] PROGMEM =  "$(\"#net\").click(function(e){e.preventDefault(); $.post(url, '{\"cmd\":\"sys\", \"stat\": ' + $(\"#stat\").prop(\"checked\") + ', \"ip\": ' + $(\"#ip\").val() + '}');});";
-const char string_3[] PROGMEM =  "$(\"input[name^=z_]:radio\").change(function(){";
-const char string_4[] PROGMEM =  "var zone_radio = $(this); var zone = zone_radio.attr(\"data-zone\");";
-const char string_5[] PROGMEM =  "var zone_status = $('input:radio[name=' + zone_radio.attr(\"name\") + ']:checked').val();";
-const char string_6[] PROGMEM =  "$.post(url,'{\"cmd\":\"zone\", \"zone\": ' + zone + ', \"status\": ' + zone_status + '}');});});";
-const char string_7[] PROGMEM = "</script></head><body style=\"background-color:#E8E8E8;\"><center><h1>Little Victories</h1><p style=\"background-color:black;\"><br></p>";
-const char string_8[] PROGMEM = "<h2>System Settings</h2><table><tr><td>";
-const char string_9[] PROGMEM = "DHCP:</td><td><input type=\"radio\" id=\"dhcp\" checked name =\"net1\" value=\"true\"><br></td></tr>";
-const char string_10[] PROGMEM = "<tr><td>Static</td><td><input type=\"radio\" id=\"stat\" name=\"net1\" value=\"true\"><input type=\"text\" id=\"ip\"></td></tr><br>";
-const char string_11[] PROGMEM = "</table><button id=\"net\">Apply Network Settings</button>";
-const char string_12[] PROGMEM = "<p style=\"background-color:black;\"><br></p><center><h2>Zone Settings</h2></center>";
-const char string_13[] PROGMEM = "Zone 1:<input type=\"radio\" data-zone=\"0\" value=\"1\" name=\"z_0\">On<input type=\"radio\" data-zone=\"0\" value=\"0\" name=\"z_0\" checked>Off<br>";
-const char string_14[] PROGMEM = "Zone 2:<input type=\"radio\" data-zone=\"1\" value=\"1\" name=\"z_1\">On<input type=\"radio\" data-zone=\"1\" value=\"0\" name=\"z_1\" checked>Off<br>";
-const char string_15[] PROGMEM = "Zone 3:<input type=\"radio\" data-zone=\"2\" value=\"1\" name=\"z_2\">On<input type=\"radio\" data-zone=\"2\" value=\"0\" name=\"z_2\" checked>Off<br>";
-const char string_16[] PROGMEM = "Zone 4:<input type=\"radio\" data-zone=\"3\" value=\"1\" name=\"z_3\">On<input type=\"radio\" data-zone=\"3\" value=\"0\" name=\"z_3\" checked>Off<br>";
-const char string_17[] PROGMEM = "Zone 5:<input type=\"radio\" data-zone=\"4\" value=\"1\" name=\"z_4\">On<input type=\"radio\" data-zone=\"4\" value=\"0\" name=\"z_4\" checked>Off<br>";
-const char string_18[] PROGMEM = "Zone 6:<input type=\"radio\" data-zone=\"5\" value=\"1\" name=\"z_5\">On<input type=\"radio\" data-zone=\"5\" value=\"0\" name=\"z_5\" checked>Off<br>";
-const char string_19[] PROGMEM = "Zone 7:<input type=\"radio\" data-zone=\"6\" value=\"1\" name=\"z_6\">On<input type=\"radio\" data-zone=\"6\" value=\"0\" name=\"z_6\" checked>Off<br>";
-const char string_20[] PROGMEM = "Zone 8:<input type=\"radio\" data-zone=\"7\" value=\"1\" name=\"z_7\">On<input type=\"radio\" data-zone=\"7\" value=\"0\" name=\"z_7\" checked>Off<br></center>";
-const char string_21[] PROGMEM = "</body></html>";
 
-const char *config_page[] = {
-  string_0,
-  string_1,
-  string_2,
-  string_3,
-  string_4,
-  string_5,
-  string_6,
-  string_7,
-  string_8,
-  string_9,
-  string_10,
-  string_11,
-  string_12,
-  string_13,
-  string_14,
-  string_15,
-  string_16,
-  string_17,
-  string_18,
-  string_19,
-  string_20,
-  string_21
-};
 
 byte mac[] = {
   0,0,0,0,0,0};
 byte ip[] = {
   192, 168, 0, 5};
+IPAddress multi(239,255,255,250);
+int multi_port = 1900;
+  
 byte zones[] = {
   1, 2, 4, 8, 16, 32, 64, 128};
 byte zStatus[] = {
   false, false, false, false, false, false, false, false};
 char buffer[200];
+char packetBuffer[50];
+char  ReplyBuffer[] = "http/1.1 200 ok\r\nST: zerolimits:sprinkler\r\n\r\n";  
 char cBuffer[5];
 char cmdBuffer[50];
 byte currentState = 0;
 EthernetClient client;
 EthernetServer server(80);
+EthernetUDP udp;
 boolean isStatic = false;
 const int net_config = 0;
 const int mac_addy = 1;
@@ -128,11 +89,13 @@ void setup(){
    }
   }
   server.begin();
+  udp.beginMulti(multi, multi_port, (uint8_t) 15);
   delay(3000);
   Serial.println(Ethernet.localIP());
 }
 
 void loop(){
+  handleSsdp();
   client = server.available();
   //If the client is connected we should see what they want
   if(client){
@@ -161,7 +124,7 @@ void loop(){
           else if(strcmp(cBuffer, "GET" == 0)){
             Serial.println("found GET");
             sendHeader(client);
-            sendMain(client);
+            client.println("Got your request bro");
           }
           Serial.println("a");
         }//END FINDER
@@ -243,18 +206,33 @@ void updateStatus(int op, boolean state)
   digitalWrite(latch, HIGH);
 }
 
-void sendMain(EthernetClient client){
-  client.print("<html>");
-  client.println("<body>");
-  client.println("<form name=""net_input"" action=""/");
-  client.println(""" method='post'>");
-  for(int i = 0; i < 21; i++)
+void handleSsdp()
+{
+   int packetSize = udp.parsePacket();
+  if (packetSize)
   {
-    strcpy_P(buffer, (char*)pgm_read_word(&(config_page[i])));
-    client.println( buffer );
+    Serial.print("Recieved SSDP");
+    Serial.print("From ");
+    IPAddress remote = udp.remoteIP();
+    for (int i = 0; i < 4; i++)
+    {
+      Serial.print(remote[i], DEC);
+      if (i < 3)
+      {
+        Serial.print(".");
+      }
+    }
+    
+    // read the packet into packetBufffer
+    udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    Serial.println(packetBuffer);
+
+    // send a reply, to the IP address and port that sent us the packet we received
+    udp.beginPacket(udp.remoteIP(), multi_port);
+    udp.write(ReplyBuffer);
+    udp.endPacket();
   }
-  delay(5);
-  client.stop();
+  delay(10);
 }
 
 void resetArray()
